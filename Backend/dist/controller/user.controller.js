05ebb8986dab4e72482ea189be69d3b04e40d283
shubham <shubs12@gmail.com>
2021-09-23 22:29:33 +0530

    password validation done

--- Backend/dist/controller/user.controller.js
+++ Backend/dist/controller/user.controller.js
@@ -8,7 +8,15 @@ const user_model_1 = __importDefault(require("../model/user.model"));
 // console.log(registerModel);
 const router = (0, express_1.Router)(); // this will allow us to register middleware;
 router.post("/", async (req, res) => {
-    const register = await user_model_1.default.create(req.body);
-    return res.status(201).json({ data: register });
+    const data = req.body;
+    if (data.password !== data.confirmPassword)
+        return res.status(400).send({ message: "your password does not match" });
+    try {
+        const register = await user_model_1.default.create(req.body);
+        return res.status(201).json({ data: register });
+    }
+    catch (err) {
+        return res.status(500).json({ message: "something went wrong" });
+    }
 });
 exports.default = router;
