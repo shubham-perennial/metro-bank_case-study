d42a5552c189e3308b20785a2d84796fb57fdcd6
shubham <shubs12@gmail.com>
2021-09-26 00:06:02 +0530

    routes seperated

--- Backend/dist/index.js
+++ Backend/dist/index.js
@@ -5,13 +5,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors = require("cors");
+const user_routes_1 = __importDefault(require("./routes/user.routes"));
 const user_controller_1 = require("./controller/user.controller");
 const app = (0, express_1.default)();
 app.use(cors());
 app.use(express_1.default.json());
+// const hello = () => {
+//   console.log("hello");
+// }
+app.use("/user", user_routes_1.default);
+// app.post("/user", hello);
 app.post("/register", user_controller_1.register);
 app.post("/login", user_controller_1.login);
 /// error handling middleware this will fire if any middleware before this have an error
+// this middleware will be called for every request to the app as this app.use is called without any route
 app.use((err, req, res, next) => {
     res.status(500).json({ message: err.message });
 });
--- Backend/src/controller/user.controller.ts
+++ Backend/src/controller/user.controller.ts
@@ -1,9 +1,9 @@
-import { Router, Request, Response } from "express";
+import { Request, Response } from "express";
 import User from "../model/user.model";
 // console.log(registerModel);
-const router = Router(); // this will allow us to register middleware;
 
-// structure for the object here
+// structure for the object here // here naming should be more specific
+
 interface data {
   name: string;
   email: string;
@@ -12,23 +12,22 @@ interface data {
   dateOfIncrp: string;
 }
 
-// post request handler
+// register function
 
 const register = async (req: Request, res: Response) => {
   const data: data = req.body;
   let user; // need to change this later
   if (data.password !== data.confirmPassword)
-    return res.status(400).send({ message: "your password does not match" });
-  //check if the user is already present in db
+    return res.status(400).send({ message: "your password does not match" }); // use proper status codes
+
   try {
-    user = await User.findOne({ email: req.body.email }).lean().exec();
+    user = await User.findOne({ email: req.body.email }).lean().exec(); // lean and exec explore it
   } catch (err) {
-    res.status(500).send({ message: "something went wrong" });
+    res.status(500).send({ message: "something went wrong" }); /// proper messages // describe what went wrong
   }
-  // if user is present then send error message;
+
   if (user) return res.status(400).send({ message: "user is already present" });
 
-  // if user is not present then create user;
   try {
     user = await User.create(req.body);
     return res.status(201).json({ data: user });
@@ -36,21 +35,23 @@ const register = async (req: Request, res: Response) => {
     return res.status(500).json({ message: "something went wrong" });
   }
 };
+
+// login function
+
 const login = async (req: Request, res: Response) => {
-  // const data = req.body;
   let user;
-  // check if the user is present or not
+
   try {
     user = await User.findOne({ email: req.body.email }).exec();
   } catch (err) {
     return res.status(404).send({ message: "something went wrong" });
   }
-  // if not present give error
+
   if (!user)
     return res
       .status(404)
       .send({ message: "user not found, check credentials" });
-  // if user is present check for password;
+
   if (user) {
     if (user.password === req.body.password) {
       return res.status(200).send({ message: "login successful" });
